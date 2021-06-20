4d813a1e219d12ad4ec6a2d6f4caeb22a6a9adae
Henry-Johnson-100 <hencutJohnson@gmail.com>
2021-06-19 22:20:44 -0500

    Initial commit

---
+++ ProjectManager.py
@@ -0,0 +1,276 @@
+import pickle
+import io
+from colorama import init
+from colorama import Fore, Back, Style
+
+
+class Project:
+    """Contains titles, descriptions, due dates, tags, and subprojects or tasks."""
+
+    def __init__(self, title: str, layer: int, parent_project: 'Project'):
+        self.title = title
+        self.layer = layer
+        self.parent = parent_project
+        self.subprojects = list()
+
+        self.description = str()
+        self.tags = set()
+        self.date = str()
+        if self.parent is not None:
+            self.priority = self.parent.priority
+        else:
+            self.priority = "0"
+
+    def get_title(self) -> str:
+        return self.title
+
+    def get_layer(self) -> int:
+        return self.layer
+
+    def get_description(self) -> str:
+        return self.description
+
+    def get_parent(self) -> 'Project':
+        return self.parent
+
+    def get_subprojects(self) -> list:
+        return self.subprojects
+
+    def get_priority(self) -> str:
+        return self.priority
+
+    def get_tags(self) -> str:
+        return ", ".join(self.tags)
+
+    def get_date(self) -> str:
+        return self.date
+
+    def get_priority_text_color(self) -> str:
+        """:
+            Returns the Colorama.Fore string containing the ANSI code for the color corresponding to a priority level.
+
+            Returns:
+                str: ANSI code contained in Colorama.Fore() object, a string.
+            """
+        return {
+            "0": Fore.WHITE,  # Neutral priority
+            "1": Fore.MAGENTA,
+            "2": Fore.BLUE,
+            "3": Fore.CYAN,
+            "4": Fore.GREEN,
+            "5": Fore.YELLOW,
+            "6": Fore.RED
+        }.get(self.priority)
+
+    def get_layer_prefix(self) -> str:
+        correction = 1
+        if self.layer == 0:
+            correction = 0
+        return str().join(["-"] * (self.layer * 4 - correction)) + ">"
+
+    def get_layer_description_spacing(self) -> str:
+        return str().join([" "] * (self.layer * 4))
+
+    def set_description(self, description: str) -> None:
+        self.description = str().join(
+            [" "] * (self.layer * 4)) + description  # + "\n"
+
+    def set_tag(self, tag: str) -> None:
+        self.tags.add(tag)
+
+    def unset_tag(self, tag: str) -> None:
+        if tag in self.tags:
+            self.tags.remove(tag)
+
+    def set_date(self, date: str) -> None:
+        self.date = date
+
+    def set_priority(self, priority: str) -> None:
+        """:
+            Set's priority attribute of a branch. Between 0 and 6.
+
+            Args:
+                priority (str): A string of the integer indicating the priority.
+            """
+        priority = str(priority)
+        lower = "0"
+        upper = "6"
+        if priority < lower:
+            priority = lower
+        if priority > upper:
+            priority = upper
+        self.priority = priority
+
+    def clear_project(self) -> 'Project':
+        """:
+            Clears, or removes, a branch by removing it from its parent's index.
+
+            Returns:
+                Project: The parent branch of the cleared branch.
+            """
+        parent_project = self.parent
+        self.parent.subprojects.remove(self)
+        return parent_project
+
+    # found a workaround in vscode to hide doc_strings
+    def def_subproject(self, title: str) -> 'Project':
+        """:
+            Creates a new Project instance nested in the self.subprojects list and one layer lower.
+
+            Args:
+                title (str): Title of the new Project.
+
+            Returns:
+                Project: New Project object, initialized with the entered title and layer = self.layer + 1
+            """
+        sub_project = Project(title, self.layer + 1, self)
+        self.subprojects.append(sub_project)
+        return sub_project
+
+    def walk_tree(self, subproject_tree_list: list, top_layer: int = 0) -> list:
+        """:
+            Returns a list of Project instances and PLACEHOLDER of all the subprojects located further down the tree.
+            \nPLACEHOLDER indicate a new subproject that is only nested in the top layer.
+
+            Args:
+                subproject_tree_list (list): pass empty list: list()
+                top_layer (int, optional): leave default, will be replaced by the top layer internally. Defaults to 0.
+
+            Returns:
+                list: List of all Projects located down the tree
+            """
+        if len(subproject_tree_list) == 0:
+            top_layer = self.layer
+        subproject_tree_list.append(self)
+        for subproject in self.subprojects:
+            subproject.walk_tree(subproject_tree_list, top_layer)
+        if self.layer == top_layer:
+            return subproject_tree_list
+
+    def move_laterally(self, direction: int) -> 'Project':
+        """:
+            Moves branches laterally. 
+            Lateral movement is across the same layer. Branches are essentially just moved in the parent branch's subproject list index.
+
+            Returns:
+                Project: The parent of the branch that has been moved.
+            """
+        if direction == 0:
+            return
+        direction = int(abs(direction) / direction)
+        if self.parent is None:
+            return
+        parent = self.parent
+        current_pos = parent.subprojects.index(self)
+        next_pos = current_pos + direction
+        self.clear_project()
+        parent.subprojects.insert(next_pos, self)
+        return parent
+
+    def move_vertically(self, direction: int) -> None:
+        if direction == 0:
+            return
+        direction = int(abs(direction) / direction)
+        if direction > 0 and self.parent.parent is None:
+            return
+        elif direction < 0 and self.parent is None:
+            return
+        parent = {1: self.parent.parent, -1: self.parent}.get(direction)
+        blank_branch = parent.def_subproject("")
+        self.clear_project()
+        blank_branch.subprojects.insert(0, self)
+
+    def __str_subprojects__(self) -> str:
+        subproject_str = str()
+        for subproject in self.subprojects:
+            subproject_str += str(subproject)
+        return subproject_str
+
+    def __str_tree__(self, **kwargs) -> str:
+        """Prints the current project and each project of a lower layer."""
+        tree = self.walk_tree(list())
+        tree_str = str()
+        for subproject in tree:
+            if subproject.layer != self.layer:
+                tree_str += subproject.__str_f__(**kwargs) + "\n"
+            else:
+                tree_str += subproject.__str_f__(**kwargs) + "\n"
+        return tree_str
+
+    def __str_f__(self, **kwargs: bool) -> str:
+        """:
+            [summary]
+
+            Kwargs:
+                highlight [bool]: If the text is highlighted (False)
+                ellipsis [bool]: If description text is truncated with an ellipsis. (False)
+                priority [bool]: If priority number is displayed (False)
+                tags [bool]: Display tags (False)
+                date [bool]: Display date (False)
+
+            Returns:
+                str: [description]
+            """
+        print_str = ""
+        print_str += self.get_layer_prefix()
+
+        if kwargs.setdefault("highlight", False):
+            print_str += Back.MAGENTA
+
+        print_str += self.get_priority_text_color()
+        print_str += self.get_title()
+
+        if kwargs.setdefault("priority", False):
+            print_str += " (priority: " + str(self.get_priority()) + ")"
+        if kwargs.setdefault("tags", False):
+            print_str += " (tags: " + self.get_tags() + ")"
+        if kwargs.setdefault("date", False):
+            print_str += " (date: " + self.get_date() + ")"
+
+        print_str += Style.RESET_ALL
+
+        if len(self.get_description()) == 0:
+            descr_string = ""
+        elif kwargs.setdefault("ellipsis", False):
+            descr_string = "\n" + \
+                           self.get_description()[0: 10].rstrip() + "..."
+        else:
+            descr_string = "\n" + self.get_description()
+
+        print_str += descr_string
+        return print_str
+
+    def __str__(self):
+        return self.__str_f__(ellpisis=True)
+
+    @staticmethod
+    def __pickle__(file):
+        """returns the given project object in pickle format."""
+        pass
+
+
+def main():
+    init()
+    return Project("Notes", -1, None)
+
+
+def save(Prj: Project, filepath: str) -> None:
+    file = open(filepath, "wb")
+    pickle.dump(Prj, file)
+    file.close()
+
+
+def load(filepath: str) -> Project:
+    file = open(filepath, "rb")
+    return pickle.load(file)
+
+
+if __name__ == "__main__":
+    test = Project("test", 0, None)
+    test.set_priority("5")
+    test.set_tag("cool")
+    test.set_tag("good")
+    test.set_description(
+        "Something to describe this test branch and test some features of __print_f__()")
+    test.date = "5/29/2021"
+    print(test.__str_f__(date=True, highlight=True, ellipsis=True, tags=True))
---
+++ ProjectManagerCLI.py
@@ -0,0 +1,338 @@
+import ProjectManager as pm
+import cmd
+import os
+import sys
+
+PRJ_TOP = pm.main()
+CURRENT_PRJ = None
+CURRENT_PRJ = PRJ_TOP
+
+
+class PrjCmd(cmd.Cmd):
+    # cmd instance vars here
+    prompt = "~: "
+
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.top = pm.main()
+        self.prj = self.top
+        self.path = "G:/pythonShit/ProjectManager/"
+        self.file = str()
+        self.print_options = str()
+        self.intro = (
+            """
+        ************************************************************************
+        *                                                                      *
+        *                              TREE NOTE                               *
+        *                                                                      *
+        ************************************************************************
+        """
+        )
+
+
+
+    @staticmethod
+    def __arg_contains(arg_str: str, *contain_str: str) -> bool:
+        """:
+            Return True if a passed keyword is found in the passed argument string.
+
+            Args:
+                arg_str (str): Argument string collected from a do_...() method.
+                *contain_str (tuple: str): Strings to check for in the arg string.
+
+            Returns:
+                bool: True if any string in *contain_str is found in arg_str
+            """
+        contains = True
+        for field in contain_str:
+            if field not in arg_str:
+                contains = False
+        return contains
+
+    @staticmethod
+    def __arg_strip(arg_str: str, str_to_strip: str, delimiter_to_split: str = " ") -> dict:
+        """:
+            Return list of remaining arguments from passed arg_string
+
+            Args:
+                arg_str (str): The arg string collected from a do_...() method
+                str_to_strip (str): The string to remove from the arg string
+                delimiter_to_split (str, optional): delimiter on which to split the arg string. Defaults to " ".
+
+            Returns:
+                dict: Keys containing the stripped arguments, values are all True.
+            """
+        arg_dict = dict()
+        remaining_str = arg_str
+        if str_to_strip in arg_str and str_to_strip != "":
+            remaining_str = arg_str.replace(str_to_strip, "")
+        remaining_list = remaining_str.split(delimiter_to_split)
+        for arg_key in remaining_list:
+            arg_dict[arg_key] = True
+        return arg_dict
+
+    def __is_valid_arg(self, arg_str: str, arg_to_test: str, delimiter_to_split: str = " ") -> tuple:
+        return self.__arg_contains(arg_str, arg_to_test), self.__arg_strip(arg_str, arg_to_test, delimiter_to_split)
+
+    def __print_tree(self, **kwargs) -> None:
+        """:
+            Print method for displaying the tree with some extra options.
+
+            Arguments:
+            clear: bool (True) - clears the terminal when printing.
+            overview: bool (False) - prints the entire tree instead of the tree starting from the current branch.
+            """
+        if kwargs.setdefault("clear", True):
+            os.system('cls')
+        if kwargs.setdefault("overview", False):
+            print(self.top.__str_tree__(**kwargs))
+        else:
+            print(self.prj.__str_tree__(**kwargs))
+
+    def __is_file_set_and_arg_empty(self, arg: str) -> bool:
+        """:
+            Returns True if self.file is not empty and the passed arg is not empty.
+
+            Args:
+                arg (str): The argument collected from a do_...() method
+
+            Returns:
+                bool: True if self.file and arg are not empty
+            """
+        return len(self.file) != 0 and (len(arg) == 0 or arg.isspace())
+
+    def __list_dir(self) -> list:
+        def filter_rule(x): return x.find(".py") == -1
+        file_list = list()
+        for (dirpath, dirname, filename) in os.walk(self.path):
+            file_list.extend(filename)
+        return list(filter(filter_rule, file_list))
+
+    def __str_dir(self) -> str:
+        file_list = self.__list_dir()
+        file_str = str()
+        for thing in filter(lambda x: x.find(".py") == -1, file_list):
+            file_str += thing + "\n"
+        return file_str
+
+    def __select_from_list(self, select_list: list):
+        if len(select_list) == 0:
+            return None
+        elif len(select_list) == 1:
+            return select_list[0]
+        for i in range(0, len(select_list)):
+            print(i + 1, str(select_list[i]))
+        select_input = input("select: ")
+        if select_input.isnumeric():
+            if int(select_input) in range(1, len(select_list) + 1):
+                return select_list[int(select_input) - 1]
+        else:
+            return None
+
+    def do_new(self, arg):
+        self.prj = self.prj.def_subproject(arg)
+        self.__print_tree()
+
+    def help_new(self):
+        print(
+            "Create a new branch nested below the current branch" +
+            "\nArgs: Name of the new branch."
+        )
+
+    def do_out(self, arg):
+        if self.prj.parent.layer >= -1:
+            self.prj = self.prj.parent
+            self.__print_tree()
+        else:
+            self.__print_tree()
+
+    def help_out(self):
+        print("Move one layer out of the tree, to the previous branch.")
+
+    def do_in(self, arg):
+        subprojects = self.prj.get_subprojects()
+        down_choice = self.__select_from_list(subprojects)
+        if down_choice is None:
+            return
+        self.prj = down_choice
+        self.__print_tree()
+
+    def help_in(self):
+        print("Move one layer into the tree."
+              "\nIf multiple branches are available, a list will be presented to choose from."
+              )
+
+    def do_top(self, arg):
+        self.prj = self.top
+        self.__print_tree()
+
+    def help_top(self):
+        print("Go to the top of the tree.")
+
+    def do_description(self, arg):
+        self.prj.set_description(arg)
+        self.__print_tree()
+
+    def help_description(self):
+        print("Enter a description for the current branch.")
+
+    def do_print(self, arg):
+        if self.__arg_contains(arg, "here"):
+            self.__print_tree(
+                **self.__arg_strip(arg + self.print_options, "here"))
+        elif self.__arg_contains(arg, "file"):
+            print(self.file)
+        elif self.__arg_contains(arg, "dir"):
+            print(self.__str_dir())
+        else:
+            self.__print_tree(
+                **self.__arg_strip(arg + " overview " + self.print_options, ""))
+
+    def help_print(self):
+        print(
+            "Displays the entire tree, according to additional arguments or options set with \'config print\'"
+            "\nSee \'?config print\' for additional options."
+            "\nArguments: \'here\' - displays the tree at the current branch and all lower branches."
+            "\nfile - displays the current filename."
+            "\ndir - displays all of the current files in the directory."
+        )
+
+    def do_clear(self, arg):
+        self.prj = self.prj.clear_project()
+        self.__print_tree()
+
+    def help_clear(self):
+        print("Remove the current branch and all lower branches from the tree.")
+
+    def do_reset(self, arg):
+        self.top = pm.main()
+        self.prj = self.top
+        self.__print_tree()
+
+    def help_reset(self):
+        print("Removes all branches from the current tree.")
+
+    def do_save(self, arg: str):
+        file_name = str()
+        if self.__is_file_set_and_arg_empty(arg):
+            pm.save(self.top, self.path + self.file)
+            file_name = self.file
+        else:
+            pm.save(self.top, self.path + arg)
+            file_name = arg
+        print("Saved to " + file_name)
+
+    def help_save(self):
+        print("Saves the current tree to the file given as an argument or if no arg is given, to the current file shown by \'print file\'.")
+
+    def do_load(self, arg):
+        del self.top
+        del self.prj
+        file_name = str()
+        if self.__is_file_set_and_arg_empty(arg):
+            self.top = pm.load(self.path + self.file)
+            file_name = self.file
+        else:
+            self.top = pm.load(self.path + arg)
+            file_name = arg
+        print("Loaded from " + file_name)
+        self.prj = self.top
+        self.__print_tree(overview=True)
+
+    def help_load(self):
+        print("Loads a tree from the file given as an argument or if no arg is given, from the current file shown by \'print file\'.")
+
+    def do_file(self, arg):
+        file_name = str()
+        if len(arg) == 0 or str(arg).isspace():
+            select = self.__select_from_list(self.__list_dir())
+            if select is None:
+                return
+            self.file = select
+        else:
+            self.file = arg
+        print("file name set to " + self.file)
+
+    def help_file(self):
+        print("Sets the current file to the name given as an argument. If no arg is given, a list of files in the current directory is shown to choose from.")
+
+    def do_priority(self, arg):
+        if len(arg) == 0 or str(arg).isspace():
+            priority_dict = {
+                pm.Fore.WHITE + "Default" + pm.Style.RESET_ALL: "0",
+                pm.Fore.MAGENTA + "Low" + pm.Style.RESET_ALL: "1",
+                pm.Fore.CYAN + "Medium" + pm.Style.RESET_ALL: "3",
+                pm.Fore.YELLOW + "High" + pm.Style.RESET_ALL: "5",
+                pm.Fore.RED + "Critical" + pm.Style.RESET_ALL: "6"
+            }
+            priority_str = self.__select_from_list(list(priority_dict))
+            self.prj.set_priority(priority_dict.setdefault(priority_str, "0"))
+        else:
+            self.prj.set_priority(arg)
+        self.__print_tree()
+
+    def help_priority(self):
+        print("Sets the priority of the branch, giving it an integer value and a text color"
+              "\nArgs:"
+              "\n(int) 0-6"
+              "\nNone - a list will be presented with options to choose from."
+              )
+
+    def do_move(self, arg):
+        if arg == "up" or arg == "down":
+            self.prj.move_laterally({"up": -1, "down": 1}.get(arg))
+        elif arg == "in" or arg == "out":
+            # self.prj.move_vertically({"in": -1, "out": 1}.get(arg)) #This needs some work
+            pass
+        else:
+            return
+
+    def help_move(self):
+        print("Moves a branch, can only move to locations which are under the same branch as the original location"
+              "\nup - move branch upwards."
+              "\ndown = move branch downwards."
+              )
+
+    def do_tag(self, arg):
+        if self.__arg_contains(arg, "remove"):
+            remove_list = list(self.__arg_strip(arg, "remove").keys())
+            for tag in remove_list:
+                self.prj.unset_tag(tag)
+        else:
+            self.prj.set_tag(arg)
+        self.__print_tree(tags = True)
+
+    def help_tag(self):
+        print("Set a tag to the current branch.")
+
+    def do_date(self, arg):
+        self.prj.set_date(arg)
+
+    def help_date(self):
+        print("Set a date to the current branch.")
+
+    def do_search(self, arg):
+        search_tags = self.__arg_strip(arg, "")
+
+    def do_filter(self, arg):
+        pass
+
+    def do_config(self, arg):
+        if self.__arg_contains("print"):
+            self.print_options = str(arg).replace("print", "")
+
+    def help_config(self):
+        print("various configurations - fill in at a later date")
+
+    def do_quit(self, arg):
+        sys.exit()
+
+    def help_quit(self):
+        print("Quits")
+
+    def emptyline(self):
+        return
+
+
+CLI = PrjCmd()
+CLI.cmdloop()
